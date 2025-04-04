c807ba6b5ec2131e44b487f20b1152bda13b1e11
d1rknwh1te3 <kirillstarodub310@gmail.com>
2025-04-04 16:42:59 +0300

    Remove removed endpoint

--- OpenDota.NET/Routes/Status/StatusEndpoint.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OpenDotaDotNet.Routes.Status;
-
-/// <inheritdoc />
-public class StatusEndpoint(Requester requester) : IStatusEndpoint
-{
-	/// <inheritdoc />
-	public Task<ServiceStatistics?> GetServiceStatisticsAsync() =>
-		requester.GetResponseAsync<ServiceStatistics>("status");
-}
